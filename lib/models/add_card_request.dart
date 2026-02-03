dbe52ea2a3005c0a33dfc43dea52f32e8e22c286
nadun097 <nadunanjana31@gmail.com>
2026-02-03 10:43:42 +0530

    Add models for card, authentication, and API responses

--- lib/models/add_card_request.dart
+++ lib/models/add_card_request.dart
@@ -1,22 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'add_card_request.g.dart';
+
+@JsonSerializable()
 class AddCardRequest {
   final String cardNumber;
   final String cvv;
-  final String expiry;
-  final bool setAsDefault;
+  final String expiry; // MM/YY format
+  final String? nickname;
+  final bool? setAsDefault;
 
   AddCardRequest({
     required this.cardNumber,
     required this.cvv,
     required this.expiry,
-    required this.setAsDefault,
+    this.nickname,
+    this.setAsDefault = false,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      'cardNumber': cardNumber,
-      'cvv': cvv,
-      'expiry': expiry,
-      'setAsDefault': setAsDefault,
-    };
-  }
+  factory AddCardRequest.fromJson(Map<String, dynamic> json) =>
+      _$AddCardRequestFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AddCardRequestToJson(this);
 }
